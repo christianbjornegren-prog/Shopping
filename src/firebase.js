cb074477b4e7eda6c55979a291b3fb45123917c5
christianbjornegren-prog <christian.bjornegren@gmail.com>
2026-02-11 20:04:43 +0000

    Add Firebase authentication - Google login working

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1Ss1RQo5jhVECbrl81vkCe2zE_E0tqxE",
+  apiKey: "AIzaSyA1SslRQo5jhVECbrl81vkCe2zE_EOtqxE",
   authDomain: "shopping-list-dev-319dd.firebaseapp.com",
   projectId: "shopping-list-dev-319dd",
   storageBucket: "shopping-list-dev-319dd.firebasestorage.app",
   messagingSenderId: "865167112779",
-  appId: "1:865167112779:web:39488e5b2276b993492bce"
+  appId: "1:865167112779:web:3b252b17398c48f0492bce"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize services
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
