1e9625e318b54b1e7629b8eb112e605a05754339
christianbjornegren-prog <christian.bjornegren@gmail.com>
2026-02-15 19:56:49 +0000

    Add environment variables for dev/prod separation

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1SslRQo5jhVECbrl81vkCe2zE_EOtqxE",
-  authDomain: "shopping-list-dev-319dd.firebaseapp.com",
-  projectId: "shopping-list-dev-319dd",
-  storageBucket: "shopping-list-dev-319dd.firebasestorage.app",
-  messagingSenderId: "865167112779",
-  appId: "1:865167112779:web:3b252b17398c48f0492bce"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
